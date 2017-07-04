703d88ed3bb7e5c97d7ab3ad31514bbfac76d845
Edmond <edmondcheung604@hotmail.com>
2017-07-04 04:41:36 +0000

    Added hard-coded values

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 var request = require('request');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Results:", result);
+});
+
 console.log('Welcome to the GitHub Avatar Downloader!');
